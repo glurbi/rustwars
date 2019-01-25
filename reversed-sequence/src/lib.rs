2a1ac2eb063819df751b990124e68687dbed6a33
Vincent Gay <glurbi@gmail.com>
2019-01-25 16:01:19 +0100

    reversed-sequence

---
+++ reversed-sequence/src/lib.rs
@@ -0,0 +1,16 @@
+// https://www.codewars.com/kata/reversed-sequence/
+
+#[allow(dead_code)]
+fn reverse_seq(n: u32) -> Vec<u32> {
+    (1..n+1).rev().collect()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn sample_test() {
+        assert_eq!(reverse_seq(5), [5, 4, 3, 2, 1].to_vec());
+    }
+}
